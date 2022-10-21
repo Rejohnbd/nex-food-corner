53788896350ce8626255a735069970c7d1f4b707
rejohnbd <rejohn.cse@gmail.com>
2022-10-21 22:28:05 +0600

    Add Slider Section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Featured from "../components/Featured";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -10,6 +12,7 @@ export default function Home() {
         <meta name="description" content="Best pizza shop in dhaka" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Featured />
       Homepage
     </div>
   );
